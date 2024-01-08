03305f59d952a003a85deacbe52cab5f0744f6ca
Udo <hoffmann.odu@gmail.com>
2024-01-08 20:34:22 +0100

    initial

---
+++ lua/mapvas/init.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+local function send_mapcat(content)
+	local cmd = ("echo '$content' | mapcat"):gsub("$content", content)
+	os.execute(cmd)
+end
+
+local function send_line(line_number)
+	if line_number == nil then
+		line_number = select(1, unpack(vim.api.nvim_win_get_cursor(0)))
+	end
+	local line_content = vim.api.nvim_buf_get_lines(0, line_number - 1, line_number, false)
+	send_mapcat(line_content)
+end
+
+M.draw = send_mapcat
+M.send_line = send_line
+return M
