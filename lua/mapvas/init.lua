b9025c1f73575283aecbf1478dc33057dd1741ec
Udo <hoffmann.odu@gmail.com>
2024-01-08 23:25:40 +0100

    test

--- lua/mapvas/init.lua
+++ lua/mapvas/init.lua
@@ -6,9 +6,11 @@ local function send_mapcat(content)
 end
 
 local function send_line(line_number)
+	vim.print(line_number)
 	if line_number == nil then
 		line_number = select(1, unpack(vim.api.nvim_win_get_cursor(0)))
 	end
+	vim.print(line_number)
 	local line_content = vim.api.nvim_buf_get_lines(0, line_number - 1, line_number, false)
 	send_mapcat(line_content)
 end
